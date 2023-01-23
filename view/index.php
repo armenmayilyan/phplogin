d2d1b479d7a52f1daedce9024699f84450cd804c
david <vardanyandavid1991@gmail.com>
2023-01-23 18:50:45 +0400

    index

--- view/index.php
+++ view/index.php
@@ -5,7 +5,7 @@ require '../model/user.php';
 session_start();
 use user as user;
 
-$user = user::getById($_SESSION['id']);
+$user = user::getWhere($_SESSION['id']);
 echo 'hello '.'  ' .$user['name'];
 
 include "./footer/footer.php";
